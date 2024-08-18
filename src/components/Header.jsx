c23afbf7ff7e2c1230d71e380b2b89133cafde19
Doniyor <donyziyayev1989@gmail.com>
2024-08-18 12:13:41 +0500

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <header>
+      <Container>Header</Container>
+    </header>
+  );
+};
+
+export default Header;
